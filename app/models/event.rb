56f54b3a6ca348bd194a2c33ebb4c9b4c3e29d7e
Tayyab064 <muhammad.tayyabmukhtar@yahoo.com>
2016-04-14 15:23:04 +0500

    geo code

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ActiveRecord::Base
 	validates_length_of :description, maximum: 500
+	geocoded_by :addre, :latitude  => :lat, :longitude => :long
+	after_validation :geocode
+
+	def addre
+		add = [self.postal_code, self.city , self.country].join(" ")
+		p add
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411070113) do
+ActiveRecord::Schema.define(version: 20160414095335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20160411070113) do
     t.integer  "postal_code", null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "lat"
+    t.float    "long"
   end
 
 end
